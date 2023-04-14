f93a72bae4919c204e1acf8e0ceaa71278ac2d3b
Cherkovetc Denis Olegovich <cherkovetsd@niuitmo.ru>
2023-04-14 16:21:12 +0300

    fixed lab1

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -1,38 +1,37 @@
-
-let hash = document.location.hash
+let hash = document.location.hash;
 let button_number;
-if (document.location.pathname.indexOf("posts.html") != -1) {
-    switch (hash) {
-        case "":
-            button_number = 0;
-            break;
-        case "#personal_info":
-            button_number = 1;
-            break;
-        case "#lifestyle":
-            button_number = 2;
-            break;
-    }
-} else if (document.location.pathname.indexOf("gallery.html") != -1) {
-    switch (hash) {
-        case "":
-            button_number = 3;
-            break;
-        case "#code":
-            button_number = 4;
-            break;
-        case "#painting":
-            button_number = 5;
-            break;
-        case "#cat":
-            button_number = 6;
-            break;
-        case "#shirt":
-            button_number = 7;
-            break;
-    }
+if (document.location.pathname.indexOf('posts.html') != -1) {
+  switch (hash) {
+    case '':
+      button_number = 0;
+      break;
+    case '#personal_info':
+      button_number = 1;
+      break;
+    case '#lifestyle':
+      button_number = 2;
+      break;
+  }
+} else if (document.location.pathname.indexOf('gallery.html') != -1) {
+  switch (hash) {
+    case '':
+      button_number = 3;
+      break;
+    case '#code':
+      button_number = 4;
+      break;
+    case '#painting':
+      button_number = 5;
+      break;
+    case '#cat':
+      button_number = 6;
+      break;
+    case '#shirt':
+      button_number = 7;
+      break;
+  }
 } else {
-    button_number = 8;
+  button_number = 8;
 }
-console.log(document.getElementsByClassName("button")[8].textContent)
-document.getElementsByClassName("button")[button_number].style.color = "black";
+console.log(document.getElementsByClassName('button')[8].textContent);
+document.getElementsByClassName('button')[button_number].style.color = 'black';
